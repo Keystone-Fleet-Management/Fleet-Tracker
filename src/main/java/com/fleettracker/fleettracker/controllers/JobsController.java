c07be70bfc1b75409bc4e16b95f57ed32aa6e0ae
cbsimpson <cbsimpson87@outlook.com>
2022-01-06 19:33:41 -0600

    merged master, Authentication and Chris_Working_Copy.

--- src/main/java/com/fleettracker/fleettracker/controllers/JobsController.java
+++ src/main/java/com/fleettracker/fleettracker/controllers/JobsController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class AddJobsController {
+public class JobsController {
 
     @GetMapping
     public String home() {
